904f09214f363fd07b90a2ed2536c2f3e5573951
clavinsh <klavins.ar@gmail.com>
2023-11-26 21:14:36 +0200

    Reworked website blocker to add FW rule for all received ip addresses

---
+++ TestProject/ConfigurationPacketTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    [TestClass()]
+    public class ConfigurationPacketTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ConfigPacket_SetsAllAndEmptyList_InvalidOperationException()
+        {
+            ConfigurationPacket.BlockedWebsites blockedWebsites = new()
+            {
+                BlockAllSites = true,
+                BlockedWebsiteUrls = new List<string>()
+            };
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ConfigPacket_SetsAllAndFilledList_InvalidOperationException()
+        {
+            ConfigurationPacket.BlockedWebsites blockedWebsites = new()
+            {
+                BlockAllSites = true,
+                BlockedWebsiteUrls = new List<string>
+                {
+                    "example.com"
+                }
+            };
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ConfigPacket_SetsEmptyListAndAll_InvalidOperationException()
+        {
+            ConfigurationPacket.BlockedWebsites blockedWebsites = new()
+            {
+                BlockedWebsiteUrls = new List<string>(),
+                BlockAllSites = true,
+            };
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void ConfigPacket_SetsFilledListAndAll_InvalidOperationException()
+        {
+            ConfigurationPacket.BlockedWebsites blockedWebsites = new()
+            {
+                BlockedWebsiteUrls = new List<string>
+                {
+                    "example.com"
+                },
+                BlockAllSites = true
+            };
+        }
+    }
+}
